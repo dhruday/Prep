498d81c3f617845e5e6f92d02e056ded1ee66276
hrudayd <hrudayd1541@gmail.com>
2025-09-17 19:50:35 +0530

    chore: move taskify_project outside Prep folder

--- Sample Project/taskify_project/taskify_project/frontend/README.md
+++
@@ -1,7 +0,0 @@
-# Taskify Frontend
-
-## Setup
-
-1. Install dependencies: `npm install`
-2. Start dev server: `npm start`
-3. The app expects the backend API at `http://localhost:5000/api/tasks`.
--- Sample Project/taskify_project/taskify_project/frontend/src/api/taskApi.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const BASE = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/tasks';
-
-export const getTasks = (params) => axios.get(BASE, { params });
-export const getTask = (id) => axios.get(`${BASE}/${id}`);
-export const createTask = (data) => axios.post(BASE, data);
-export const updateTask = (id, data) => axios.put(`${BASE}/${id}`, data);
-export const deleteTask = (id) => axios.delete(`${BASE}/${id}`);
